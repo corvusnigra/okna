cddd04e301744108c38461e9d57d0aa7d74cd819
ymarkov <itemruby@gmail.com>
2019-06-24 16:37:21 +0300

    add slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,20 @@
 $(function() {
 
-	// Custom JS
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:5
+			}
+		}
+	})
 
 });
