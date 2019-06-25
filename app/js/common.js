bb9ff617faaf7bf56a1a1b6a532c863ffd40580f
ymarkov <itemruby@gmail.com>
2019-06-25 19:15:53 +0300

    add slider block

--- app/js/common.js
+++ app/js/common.js
@@ -5,17 +5,7 @@ $(function() {
 		loop:true,
 		margin:10,
 		nav:true,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:3
-			},
-			1000:{
-				items:5
-			}
-		}
+		items: 1
 	})
 
 
