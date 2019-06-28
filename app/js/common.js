31fdb710491a995ef83a9d431da27aef990e99a7
ymarkov <itemruby@gmail.com>
2019-06-28 19:48:24 +0300

    add tabs section

--- app/js/common.js
+++ app/js/common.js
@@ -21,4 +21,15 @@ $(function() {
 		$(".mobile-menu").hide();
 	  });
 
+
+	  $('.tab-title li').not('.active').click(function(){
+		var index = $(this).index();
+		var content = $('.tab-content li').eq(index);
+		$(this).addClass('active').siblings().removeClass('active');
+		$('.tab-content li').css('display', 'none').eq(index).css('display', 'block');
+	})
+	
+		$('.tab-title li:first').addClass('active');
+		$('.tab-content li:first').css('display', 'block');
+
 });
